519c67e393b18ad3925e86c9b7dff9c1d3cbd58a
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-01-05 22:13:53 +0530

    Create 11.py

---
+++ lab_programs/11.py
@@ -0,0 +1,15 @@
+def main():
+    list = [1, 4, 5, 11, 98, 3, 0, 7]
+    print(list)
+    bubbleSort(list)
+    print(list)
+
+
+def bubbleSort(list):
+    for i in range(len(list)):
+        for j in range(len(list) - 1 - i):
+            if (list[j] > list[j+1]):
+                list[j], list[j + 1] = list[j + 1], list[j]
+
+
+main()
