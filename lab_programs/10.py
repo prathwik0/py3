81a5d88d448b990057e8a28a9cb56d10fd066a90
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-01-06 05:42:36 +0530

    Update 10.py

--- lab_programs/10.py
+++ lab_programs/10.py
@@ -1,11 +1,14 @@
+def reverse(n):
+    n2 = n
+    nReverse = 0
+    while (n2 != 0):
+        nReverse = (nReverse * 10) + n2 % 10
+        n2 = int(n2/10)
+    return nReverse
+
+
 n = int(input("Enter a number: "))
-n2 = n
-
-nReverse = 0
-
-while (n2 != 0):
-    nReverse = (nReverse * 10) + n2 % 10
-    n2 = int(n2/10)
+nReverse = reverse(n)
 
 print("Reverse:", nReverse)
 
