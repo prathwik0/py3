0e9d117d3ee7428567c971a598a72ce6a3bb18cf
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-01-06 05:57:05 +0530

    Update 11.py

--- lab_programs/11.py
+++ lab_programs/11.py
@@ -10,6 +10,7 @@ def bubbleSort(list):
         for j in range(len(list) - 1 - i):
             if (list[j] > list[j+1]):
                 list[j], list[j + 1] = list[j + 1], list[j]
+    return list
 
 
 main()
