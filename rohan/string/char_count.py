bb228de1f0ba690370d03cb4370b047322ba21b7
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-01-09 19:48:54 +0530

    Create char_count.py

---
+++ rohan/string/char_count.py
@@ -0,0 +1,10 @@
+string = "Hello, World!"
+char_count = {}
+
+for char in string:
+    if char in char_count:
+        char_count[char] += 1
+    else:
+        char_count[char] = 1
+
+print(char_count)
