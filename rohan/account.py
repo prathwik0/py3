4bc5f3a89c15cfb16aea2912c35abad98d9becf7
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-01-13 10:27:30 +0530

    Update account.py

--- rohan/account.py
+++ rohan/account.py
@@ -1,20 +1,26 @@
 class account:
     def __init__(self, num, bal=100):
-        self.acc_num = num
-        self.balance = bal
+        self.__acc_num = num
+        self.__balance = bal
 
     def withdraw(self, amount):
-        if self.balance <= amount+100:
+        if self.__balance <= amount+100:
             print("Insufficient Balance")
         else:
-            self.balance = self.balance - amount
+            self.__balance = self.__balance - amount
 
     def deposit(self, amount):
-        self.balance = self.balance + amount
+        self.__balance = self.__balance + amount
+    
+    def getAccountNum(self):
+        return self.__acc_num
+    
+    def getBalance(self):
+        return self.__balance
 
     def display(self):
-        print("\nThe account number is :", self.acc_num)
-        print("The Account balance is :", self.balance)
+        print("\nThe account number is :", self.__acc_num)
+        print("The Account balance is :", self.__balance)
 
 
 acc_list = []
@@ -22,7 +28,7 @@ acc_list = []
 
 def new_account(num):
     for i in acc_list:
-        if i.acc_num == num:
+        if i.getAccountNum() == num:
             print("Account already present")
             return
     acc_list.append(account(num))
@@ -30,7 +36,7 @@ def new_account(num):
 
 def process(num, operation, amount):
     for i in acc_list:
-        if i.acc_num == num:
+        if i.getAccountNum() == num:
             if operation == "withdraw":
                 i.withdraw(amount)
             elif operation == "deposit":
@@ -47,11 +53,11 @@ def highest_balance():
     highest_acc = None
 
     for i in acc_list:
-        if i.balance > highest_balance:
-            highest_balance = i.balance
+        if i.getBalance() > highest_balance:
+            highest_balance = i.getBalance()
             highest_acc = i
 
-    print("Account", highest_acc.acc_num,
+    print("Account", highest_acc.getAccountNum(),
           "has the highest balance of", highest_balance)
 
 
@@ -61,7 +67,7 @@ for i in range(n):
     new_account(acc_num)
 
 while (1):
-    print("\n1)DEPOSIT\n2)WITHDRAWL\n3)DISPLAY\n4)EXIT\n")
+    print("\n1)DEPOSIT\n2)WITHDRAWL\n3)DISPLAY\n4)HIGHEST_BAL\n")
     ch = int(input("Enter your choice:"))
 
     if ch == 1:
@@ -80,9 +86,6 @@ while (1):
 
     elif ch == 4:
         highest_balance()
-
-    elif ch == 5:
+        
+    else: 
         quit()
-
-    else:
-        print("Invalid choice")
