ebb3c245d210cbb9c7873abc11d70d2a47b9b327
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-01-29 22:09:34 +0530

    Create 14.py

---
+++ lab_programs/14.py
@@ -0,0 +1,6 @@
+# Write an Object oriented Python program to create two Time objects:
+# currentTime, which contains the current time; and breadTime, which
+# contains the amount of time it takes for a bread maker to make bread.
+# Then we'll use addTime to figure out when the bread will be done.
+# Write the printTime function to display the time when the bread will
+# be done by the bread maker.
