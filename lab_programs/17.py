c270d0228387a05c8672c0555cd872bb2d294cdc
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-01-29 17:52:33 +0530

    Update 17.py

--- lab_programs/17.py
+++ lab_programs/17.py
@@ -1,3 +1,25 @@
+# Develop python program to perform the below mentioned operations.
+
+# a) display total marks scored by each student
+# b) Display top scorer and the top score
+
+# Scenario: There are 8 students and answers to 10 multiple
+# choice questions of each student is stored in a file
+# called marks.txt. Each answer is delimited by space. Each
+# line provides a student’s answers to the questions, as
+# shown below.The answer key isstored in a file named keys.txt.
+# The format of answer keys as shown below.
+
+# Key D B D C C D A E A D
+# Student0 A B A C C D E E A D
+# Student1 D B A B C A E E A D
+# Student2 E D D A C B E E A D
+# Student3 C B A E D C E E A D
+# Student4 A B D C C D E E A D
+# Student5 B B E C C D E E A D
+# Student6 B B A C C D E E A D
+# Student7 E B E C C D E E A D
+
 fileName = 'marks.txt'
 keyFileName = 'key.txt'
 
