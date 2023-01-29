017e7a8e838ba7740c3a7f3f6f9bb3d2eb66bb30
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-01-29 22:09:29 +0530

    Update 12.py

--- lab_programs/12.py
+++ lab_programs/12.py
@@ -1,3 +1,13 @@
+# Write a function that returns the index of the smallest element
+# in a list of integers. If the number of such elements is greater
+# than 1,return the smallest index. Use the following header:
+
+# def indexOfSmallestElement(lst):
+
+# Write a test program that prompts the user to enter a list of
+# numbers, invokes this function to return the index of the smallest
+# element and displays the index.
+
 def indexOfSmallestElement(list):
     index = 0
     for i in range(len(list)):
